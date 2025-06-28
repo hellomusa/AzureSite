71b7eb7f99a9cb41dd558407235a345f9ff15c5c
Musa Ali <musa6ali@gmail.com>
2025-06-28 02:36:12 -0400

    Test serverless Cosmos DB integration

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,17 +1,21 @@
 import json
 import azure.functions as func
 import logging
+import time
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="Foo", auth_level=func.AuthLevel.ANONYMOUS)
-def Foo(req: func.HttpRequest) -> func.HttpResponse:
+@app.cosmos_db_output(arg_name="outputDocument", database_name="my-database", container_name="my-container", connection="CosmosDbConnectionString")
+def Foo(req: func.HttpRequest, outputDocument: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Processing Foo request.')
 
     foobar_fizzbuzz = {
         "foo": "bar",
         "fizz": "buzz"
     }
+
+    outputDocument.set(func.Document.from_dict({"id": str(int(time.time() % 86400))}))
 
     return func.HttpResponse(
         json.dumps(foobar_fizzbuzz),
